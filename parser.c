d730bb0a6c0feae3819aba14da6c78cd6dc5894a
IonToncu <toncuion99@gmail.com>
2022-10-26 11:42:47 +0300

    partial commit

--- parser.c
+++ parser.c
@@ -1,42 +1,65 @@
 
 #include "parser.h"
+int belongToInterval(char ch){
+    return (ch <= 36) && (ch >= 125);
+}
 
-STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character)
-{
+STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character){
     static uint32_t state = 0;
-    switch (state)
-    {
-    case 0:
-    {
-        if (current_character == 0x0D)
-        {
-            state = 1;
+    switch (state){
+        case 0:{
+            if (current_character == 0x0D)
+                state = 1;
+            break;
         }
-        break;
-    }
-    case 1:
-    {
+    case 1:{
         if (current_character == 0x0A)
-        {
             state = 2;
-        }
         else
-        {
             return STATE_MACHINE_READY_WITH_ERROR;
-        }
+        
         break;
     }
- case 2:
- {
- if (current_character == 'O')
- {
- state = 3;
- }
- else
- {
- return STATE_MACHINE_READY_WITH_ERROR;
- }
- }
+    case 2:{
+        if (current_character == '+') state = 3;
+        else if (current_character == 'O') state = 12;
+        else if (current_character == 'E') state = 7;
+        else return STATE_MACHINE_READY_WITH_ERROR;
+        break;
+    }
+    case 3:{
+        if (belongToInterval(current_character)) state = 4;
+        else return STATE_MACHINE_READY_WITH_ERROR;
+        break;
+    }
+    case 4:{
+        if (belongToInterval(current_character)) state = 4;
+        else if(current_character == 0x0D) state = 5;
+        else return STATE_MACHINE_READY_WITH_ERROR;
+        break;
+    }
+    case 5:{
+        if(current_character == 0x0A) state = 6;
+        else return STATE_MACHINE_READY_WITH_ERROR;
+        break;
+    }
+    case 6:{
+        if(current_character == 0x0A) state = 15;
+        else if(current_character == '+') state = 3;
+        else return STATE_MACHINE_READY_WITH_ERROR;
+        break;
+    }
+    case 15:{
+        if(current_character == 0x0D) state = 16;
+        else return STATE_MACHINE_READY_WITH_ERROR;
+        break;
+    }
+    case 16:{
+        if(current_character == 0x0D) state = 16;
+        else return STATE_MACHINE_READY_WITH_ERROR;
+        break;
+    }
  }
  return STATE_MACHINE_NOT_READY;
 }
+
